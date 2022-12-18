8d5cca259528cce00048096b0c06f7bfbbf04cfd
Manmohandeep Singh <ltsbcsgo@gmail.com>
2022-12-17 20:45:58 -0500

    commit tree 24ebd34441df0dafd1e798d875cae0a4dac5ab47

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let reply;
+
+  if(feet <= 400) {
+    reply = 'This one is on me!';
+  }
+  else if (feet > 400 && feet <2000) {
+    reply = 'That will be twenty bucks.';
+  }
+  else if (feet> 2000 && feet <2500) {
+    reply = 'I will gladly take your thirty bucks.';
+  }
+  else if (feet > 2500) {
+    reply = 'No can do.';
+  }
+  return reply;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  let reply; 
+  if(city == "NYC") {
+    reply = "Ok, sounds good.";
+  }
+  else if  (city != "NYC") {
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+    reply = "No go.";
+  }
+  return reply;
+  }
+
+
+function switchOnCharmFromTip(tip){
+ let reply;
+ switch(tip) {
+  case 'generous':
+    reply = "Thank you so much.";
+    break;
+  case 'not as generous':
+    reply = "Thank you.";
+    break;
+  default:
+    reply = 'Bye.'
+    break;
+ }
+ return reply;
+ }
+
+
+
